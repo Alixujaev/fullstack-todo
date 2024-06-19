4530339c68d6e702af33deaaca6a7f51c021604a
islom <alixujaevislom@gmail.com>
2024-06-19 23:35:58 +0500

    bug fixed

--- client/src/components/TodoForm.tsx
+++ client/src/components/TodoForm.tsx
@@ -12,7 +12,7 @@ const TodoForm = () => {
     e.preventDefault();
 
     if (inputRef.current) {
-      fetch(`https://fullstack-todo-blond.vercel.app/todo`, {
+      fetch(`http://localhost:8000/todo`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
